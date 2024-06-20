5bff3920b802fb061734291725bb699b90251271
Helder Fernandes Barbosa <helderrabino34@gmail.com>
2024-06-20 20:43:37 -0300

    commit inicial

---
+++ src/tarefa.js
@@ -0,0 +1,15 @@
+const alunos = [
+    {nome: 'João', nota: 7},
+    {nome: 'Maria', nota: 5},
+    {nome: 'Pedro', nota: 8},
+    {nome: 'Ana', nota: 4},
+    {nome: "Carlos", nota: 6}
+];
+
+function alunosAprovados(arrayDeAlunos) {
+    return arrayDeAlunos.filter(aluno => aluno.nota >= 6)
+}
+
+const alunosAprovadosArray = alunosAprovados(alunos);
+console.log("Alunos aprovados:")
+console.log(alunosAprovadosArray)
